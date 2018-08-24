c0feb7f75e365a75519660b65543cf27f21ba136
DianaCarrillo <39560661+DianaCarrillo@users.noreply.github.com>
2018-08-23 19:40:18 -0500

    subiendo estlint correctamente, subiendo intento de modal

--- src/js/data.js
+++ src/js/data.js
@@ -1,10 +1,12 @@
 let breakingBadBtn = document.getElementById('breaking-bad-btn');
 let whereThewildBtn = document.getElementById('where-the-btn');
 let harryPotterBtn = document.getElementById('harry-potter-btn');
-let api = `70a6af2c`;
+let api = '70a6af2c';
 let search;
 let newArray = [];
-let sectionMovies = document.getElementById('section-movies')
+let sectionMovies = document.getElementById('section-movies');
+
+$('.modal').modal();
 
 window.getMovies = (api, search) => {
   const json = `http://www.omdbapi.com/?&s=${search}&apikey=${api}`;
@@ -14,7 +16,6 @@ window.getMovies = (api, search) => {
   fetch(json)
     .then((res) => { // Aquí ya tiene la información.
       return res.json(); // Entonces le digo que a esa información la retorne como un archivo json.
-
     })
     .then((data) => { // Aquí le asigno el nombre de laboratoria a la información que envió.
       pintar(data);
@@ -26,33 +27,46 @@ window.getMovies = (api, search) => {
 
 breakingBadBtn.addEventListener('click', () => {
   search = 'breaking+bad';
-  getMovies(api, search)
+  getMovies(api, search);
 });
 
 whereThewildBtn.addEventListener('click', () => {
   search = 'where+the+wild+things+are';
-  getMovies(api, search)
-
+  getMovies(api, search);
 });
 harryPotterBtn.addEventListener('click', () => {
   search = 'Harry+potter';
-  getMovies(api, search)
-
+  getMovies(api, search);
 });
 
 
 const pintar = (data) => {
-for (i in data){
-let newArray = data[i];
-newArray.forEach(element =>{
-  console.log(element);
-  let title = element.Title;
-  let poster = element.Poster;
-  console.log(poster);
-  sectionMovies.innerHTML += `
+  sectionMovies.innerHTML = '';
+  for (i in data) {
+    let newArray = data[i];
+    newArray.forEach(element =>{
+      console.log(element);
+      let title = element.Title;
+      let poster = element.Poster;
+      console.log(poster);
+      sectionMovies.innerHTML += `
                             <p>${title}</p>
                             <img class="poster-image" src="${poster}">
-                            </div>`
-});
-}
-}
+                            </div>`;
+    });
+  }
+  sectionMovies.addEventListener('click', ()=>{
+    let containerModal = document.getElementById('container-modal');
+    let modal = `<div id="modal1" class="modal">
+               <div class="modal-content">
+              <h4>Modal Header</h4>
+              <p>A bunch of text</p>
+              </div>
+              <div class="modal-footer">
+              <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+              </div>
+              </div>`;
+    containerModal.innerHTML = modal;
+  });
+};
+
